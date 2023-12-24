0d611aa2d1e8830761c14f7ea1f7a98ac215af9d
Zeyad-3amr <zeyad.3amr01@gmail.com>
2023-12-24 23:48:31 +0200

    removed the node modules

--- client/src/instance.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-const instance = axios.create({
-  baseURL: 'https://chatify-com.onrender.com/api/chat-application',
-  // baseURL: 'http://localhost:8000/api/chat-application',
-  withCredentials: true,
-});
-
-export default instance;
--- server/src/routes/roomRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { protect } from '../controllers/userController';
-import { roomCheck, getChats } from '../controllers/roomController';
-
-export const router = express.Router();
-
-router.route('/roomCheck/:id').get(protect, roomCheck);
-router.route('/getChats').get(protect, getChats);
